9d7a94d2b1cabaf48856224c2b0addec5c5ea53f
Jeevan Gyawali <jeewangw9@gmail.com>
2020-06-11 15:26:38 -0500

    All projects done

---
+++ Project2_Phase1/Studnet_Management.sql
@@ -0,0 +1,26 @@
+/*Show Databases
+show databases;
+
+/*Create Databases
+create database student_manage;
+
+/* Use student_manage Database
+use student_manage;
+
+/*Show Tables
+show tables;
+
+/*Create Table
+CREATE TABLE STUDNETS (
+	SID int(11) PRIMARY KEY AUTO_INCREMENT,
+	SName VARCHAR(100) NOT NULL,
+    SPhone VARCHAR(12),
+    SCity VARCHAR(50)
+);
+
+/*Describe Tables
+desc STUDNETS;
+
+/*Select
+Select * from STUDNETS;
+
